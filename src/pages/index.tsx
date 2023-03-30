c67cccf87601187c6146dabed0014a6b29af7971
mohammed arshad hassan <mohammed@ubuntu.com>
2023-03-31 00:18:54 +0530

    add image to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import * as React from "react"
 import { HeadFC, Link, PageProps } from "gatsby"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage: React.FC<PageProps> = () => {
 	return (
 		<Layout pageTitle="Home page">
-				<h1>Hi, Welcome to my gatsby site!</h1>
-				<p>I'm making this by following the gatsby tutorial.</p>
-				<Link to="/about" color="green">About</Link>
+			<h1>Hi, Welcome to my gatsby site!</h1>
+			<StaticImage src="../images/dog.webp" alt="Clifford, a red bulldog, stoically looking at the camera!" />
+			<p>I'm making this by following the gatsby tutorial.</p>
+			<Link to="/about" color="green">About</Link>
 		</Layout>
 	)
 }
