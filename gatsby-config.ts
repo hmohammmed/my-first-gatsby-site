5a1ac80677785745cdf029e9367a2289151cb06b
mohammed arshad hassan <mohammed@ubuntu.com>
2023-05-07 23:51:42 +0530

    I learnt mdx

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,7 +10,7 @@ const config: GatsbyConfig = {
 	// If you use VSCode you can also use the GraphQL plugin
 	// Learn more at: https://gatsby.dev/graphql-typegen
 	graphqlTypegen: true,
-	plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp",
+	plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-plugin-mdx",
 		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -2,14 +2,18 @@ import { graphql, PageProps } from "gatsby";
 import React from "react";
 import Layout from "../components/layout";
 
-export default function Blogs(props: PageProps<Queries.BlogQuery>) {
+export default function Blogs(props: PageProps<Queries.BlogsQuery>) {
 	const { data } = props;
 	return (
 		<Layout pageTitle="Blogs">
 			<h1>Blog page</h1>
 			<ol>
-				{data.allFile.nodes.map(val => (
-					<li key={val.name}>{val.name}</li>
+				{data.allMdx.nodes.map(val => (
+					<article key={val.id}>
+						<h2>{val?.frontmatter?.title}</h2>
+						<p>{val?.frontmatter?.date}</p>
+						<p>{val.excerpt}</p>
+					</article>
 				))}
 			</ol>
 		</Layout>
@@ -17,11 +21,16 @@ export default function Blogs(props: PageProps<Queries.BlogQuery>) {
 };
 
 export const query = graphql`
-query Blog{
-	allFile{
-		nodes{
-			name
-		}
-	}
+query Blogs {
+  allMdx(sort: {frontmatter: {date: DESC}}) {
+    nodes {
+      id
+      excerpt
+      frontmatter {
+        title
+				date(formatString: "MMMM DD, YY")
+      }
+    }
+  }
 }
 `
